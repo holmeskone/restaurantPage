665b2a9761e5949fe4600ff16c998f8573fbdd4c
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-20 14:53:31 +0100

    Create function to clear or wipe out previous content and call the functions to load information of each section

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import "./styles.css"; // Import CSS styles 
-import loadPage from "./pageLoad"; //Import pageload module
+// import loadPage from "./pageLoad"; //Import pageload module
+import loadMenu from "./menu";
+import loadAbout from "./about";
+import loadPage from "./pageLoad";
+
+
 
 //Call function on pageload
-document.addEventListener('DOMContentLoaded', () => {
-    loadPage();
+const menuButton = document.getElementById('menu-button');
+const aboutButton = document.getElementById('about-button');
+const homeButton = document.getElementById('home');
+
+//Function to clear contents
+function clearContent(){
+    const content = document.getElementById('content');
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+      }
+}
+
+
+menuButton.addEventListener('click', () => {
+    clearContent();
+    loadMenu();
 }); 
+
+aboutButton.addEventListener('click', () => {
+    clearContent();
+    loadAbout();
+}); 
+
+homeButton.addEventListener('click', () => {
+    clearContent();
+    loadPage();
+});
