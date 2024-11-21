6c49ac7a25c41ae9453434d68188afd8cb1b4ad4
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-21 10:40:10 +0100

    Added changes that were in webpack.config into common

--- webpack.common.js
+++ webpack.common.js
@@ -5,11 +5,32 @@ const path = require('path');
    entry: {
      app: './src/index.js',
    },
+   devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    },
    plugins: [
      new HtmlWebpackPlugin({
        title: 'Production',
+       template: "./src/template.html",
      }),
    ],
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
    output: {
      filename: '[name].bundle.js',
      path: path.resolve(__dirname, 'dist'),
